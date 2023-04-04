75b2f37342640a8a116bfc555046e37358560e70
Dewi Kurnia 210511032 <127664566+dewwkurniaa@users.noreply.github.com>
2023-04-04 11:02:13 +0700

    Add files via upload

---
+++ tugas3/animalsound.py
@@ -0,0 +1,104 @@
+from playsound import playsound
+
+class hewan:
+    def __init__(self,hewan):
+        self.hewan = hewan
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+
+class anjing(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\anjing.mp3")
+
+class ayam(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\ayam.mp3")
+
+class babi(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\babi.mp3")
+
+class elang(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\elang.mp3")
+
+class gajah(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\gajah.mp3")
+
+class kambing(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\kambing.mp3")
+
+class kucing(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\kucing.mp3")
+
+class kuda(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\kuda.mp3")
+
+class macan(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\macan.mp3")
+
+class nyamuk(hewan):
+    def __init__(self,hewan):
+        super().__init__(hewan)
+    def bersuara(self):
+        print(f'{self.hewan} bersuara ...')
+        playsound("E:\\PBO2\\pert3\\Tugas\\suara\\nyamuk.mp3")
+
+def hewan_bersuara(hewan):
+    hewan.bersuara()
+
+hewan0 = hewan('Hewan')
+hewan1 = anjing('Anjing')
+hewan2 = ayam('Ayam')
+hewan3 = babi('Babi')
+hewan4 = elang('Elang')
+hewan5 = gajah('Gajah')
+hewan6 = kambing('Kambing')
+hewan7 = kucing('Kucing')
+hewan8 = kuda('Kuda')
+hewan9 = macan('Macan')
+hewan10 = nyamuk('Nyamuk')
+
+hewan_bersuara(hewan0)
+hewan_bersuara(hewan1)
+hewan_bersuara(hewan2)
+hewan_bersuara(hewan3)
+hewan_bersuara(hewan4)
+hewan_bersuara(hewan5)
+hewan_bersuara(hewan6)
+hewan_bersuara(hewan7)
+hewan_bersuara(hewan8)
+hewan_bersuara(hewan9)
+hewan_bersuara(hewan10)
