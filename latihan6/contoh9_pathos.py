18122e114345513ac6be77b253f5bc771f56edd3
Dewi Kurnia 210511032 <127664566+dewwkurniaa@users.noreply.github.com>
2023-05-14 20:50:57 +0700

    Add files via upload

---
+++ latihan6/contoh9_pathos.py
@@ -0,0 +1,12 @@
+print('Dewi Kurnia\n210511032\nT121A(R1)\n')
+
+from pathos.multiprocessing import ProcessPool
+
+def worker(num):
+    """Fungsi untuk menjalankan tugas pada proses multiprocessing"""
+    print('Worker', num)
+if __name__ == '__main__':
+# Buat ProcessPool dengan 2 proses
+    pool = ProcessPool(2)
+# Panggil fungsi worker pada setiap item secara parallel
+    pool.map(worker, range(5))
